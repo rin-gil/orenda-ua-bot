13df5cdbeb7342f1a84a4b995fc8b21f6452dd65
rin-gil <91410075+rin-gil@users.noreply.github.com>
2022-12-23 19:41:03 +0200

    fix: Fix scheduler time
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- tgbot/misc/sheduler.py
+++ tgbot/misc/sheduler.py
@@ -9,5 +9,5 @@ from tgbot.models.distribution import check_for_new_ads
 async def schedule(dp: Dispatcher) -> None:
     """Створює завдання розсилки нових оголошень в планувальнику"""
     scheduler: AsyncIOScheduler = AsyncIOScheduler()
-    scheduler.add_job(func=check_for_new_ads, trigger="cron", hour="*", minute="*/1", args=(dp,), timezone="UTC")
+    scheduler.add_job(func=check_for_new_ads, trigger="cron", hour="*", minute="*/15", args=(dp,), timezone="UTC")
     scheduler.start()
